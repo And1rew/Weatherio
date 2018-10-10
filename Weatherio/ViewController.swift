f3e265a2901ca1c64786f126e4487e84b036e472
Ponsumer <33775534+Ponsumer@users.noreply.github.com>
2018-10-10 19:42:14 +0500

    Initial Commit

---
+++ Weatherio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weatherio
+//
+//  Created by Andrey Snopov on 10/10/2018.
+//  Copyright © 2018 Andrey Snopov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
